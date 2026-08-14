f82aa8418f82731f0e553234c4a1eae76d2e7e0c
KauaTrombim <kauabenjamint@gmail.com>
2026-08-13 22:49:26 -0300

    exclusão de arquivos

--- setup.sh
+++ setup.sh
@@ -1,11 +1,9 @@
+#Instala dependência de áudio
+sudo apt install portaudio19-dev python3-dev gcc
 #Definindo Ambiente Virtual (Só rode na pasta certa, colega)
 sudo apt update && sudo apt install python3-venv python3-full -y
 python3 -m venv env
 source env/bin/activate
-
-###instalar pyaudio no linux
-sudo apt install portaudio19-dev python3-dev gcc
+###instala biblioteca
 pip install pyaudio
-
-###Instala Vosk
 pip install vosk
--- src/voiceRec/constantes.py
+++
@@ -1,6 +0,0 @@
-from typing import Final
-
-#Originamento te um 'r' antes do caminho
-MODEL_PATH : Final = "/home/yobenjas/Semear/Bulbatech-Semear/vosk-model-small-pt-0.3/"
-DEVICE_INDEX : Final = 4
-AUDIO_FREQUENCY = 48000
