19c95df57793cead8d105bcc4609ac994d37915c
KauaTrombim <kauabenjamint@gmail.com>
2026-08-13 23:11:55 -0300

    Ajustes finais do dia

--- src/voiceRec/voice.py
+++ src/voiceRec/voice.py
@@ -8,24 +8,36 @@ import constantes
 
 
 ##Configurando modelo
-model = Model(MODEL_PATH)
-recognizer = KaldiRecognizer(model, AUDIO_FREQUENCY)
+model = Model(constantes.MODEL_PATH)
+recognizer = KaldiRecognizer(model, constantes.AUDIO_FREQUENCY)
 ##Configurando o microfone
 microphone = pyaudio.PyAudio()
 stream = microphone.open(
     format=pyaudio.paInt16,
     channels=1,
-    rate = AUDIO_FREQUENCY,
+    rate = constantes.AUDIO_FREQUENCY,
     input=True,
-    input_device_index=DEVICE_INDEX,
-    frames_per_buffer=8192
+    input_device_index=constantes.DEVICE_INDEX,
+    frames_per_buffer=constantes.BUFFER_SIZE
     )
 ##Inicia a escuta
 stream.start_stream()
 print("Ouvindo...")
 ##Interpreta o áudio
 while True:
-    data = stream.read(4396, exception_on_overflow=False)
+    data = stream.read(constantes.BUFFER_SIZE, exception_on_overflow=False)
     if recognizer.AcceptWaveform(data):
         resultado = json.loads(recognizer.Result())
         print("Você disse:", resultado["text"])
+
+##Teste de palavras reconhecidas
+# while True:
+#     data = stream.read(constantes.BUFFER_SIZE, exception_on_overflow=False)
+
+#     if recognizer.AcceptWaveform(data):
+#         resultado = json.loads(recognizer.Result())
+#         print("FRASE FINAL:", resultado["text"])
+
+#     else:
+#         parcial = json.loads(recognizer.PartialResult())
+#         print("PARCIAL:", parcial["partial"])
