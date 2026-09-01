049d6a4b53d0af131b6bd80fa528f60485ef1dec
usp-lucas <souza.lucas2@usp.br>
2026-08-31 23:10:27 -0300

    remove scripts provisorios do rastreamento e atualiza .gitignore

--- setup.sh
+++ setup.sh
@@ -1,26 +1,18 @@
-<<<<<<< HEAD
 #Instala dependência de áudio
 sudo apt update
 sudo apt install portaudio19-dev python3-dev gcc
+#Instala dependência de vídeo
+sudo apt update && sudo apt install python3-venv python3-full libopencv-dev python3-opencv -y
 #Definindo Ambiente Virtual ------->(Só rode na pasta certa, colega)<------
 sudo apt install python3-venv python3-full -y
-=======
-#Instala dependência de áudio e vídeo
-sudo apt update && sudo apt install python3-venv python3-full libopencv-dev python3-opencv -y
-sudo apt install portaudio19-dev python3-dev gcc
-#Definindo Ambiente Virtual (Só rode na pasta certa, colega)
->>>>>>> b80fb31 (Cria módulos iniciais de Visão Computacional.)
 python3 -m venv env
 source env/bin/activate
 ###instala biblioteca
 pip install pyaudio
 pip install vosk
-<<<<<<< HEAD
 pip install sounddevice numpy
 pip install piper-tts
 pip install pydub
+pip install opencv-python
 #pip install pyaudio vosk
 #pip install sounddevice numpy piper-tts
-=======
-pip install opencv-python
->>>>>>> b80fb31 (Cria módulos iniciais de Visão Computacional.)
